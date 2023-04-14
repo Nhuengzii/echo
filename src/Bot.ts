882a3d61160260ef108d0c36ec642a2e3fadd17d
Nhuengzii <32nhueng@gmail.com>
2023-04-14 15:13:28 +0700

    do not load .env

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,26 +1,22 @@
-import {Client, GatewayIntentBits} from "discord.js"
-import { config } from "dotenv"
-import { SongQueue } from "./libs/SongQueue"
+import { Client, GatewayIntentBits } from "discord.js";
+import { SongQueue } from "./libs/SongQueue";
 
-config()
-const TOKEN = process.env.TOKEN
-const client:Client = new Client({
+const TOKEN = process.env.TOKEN;
+const client: Client = new Client({
     intents: [
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.MessageContent
-    ]
-})
+        GatewayIntentBits.MessageContent,
+    ],
+});
 
-export type QueueManager = {[guildId: string]: SongQueue}
-const queueManager: QueueManager = {}
-export {queueManager}
+export type QueueManager = { [guildId: string]: SongQueue };
+const queueManager: QueueManager = {};
+export { queueManager };
 
-
-require('./event/ready')(client)
-require('./event/onMessageCreate')(client)
-require('./event/speech')(client)
-
+require("./event/ready")(client);
+require("./event/onMessageCreate")(client);
+require("./event/speech")(client);
 
 client.login(TOKEN);
